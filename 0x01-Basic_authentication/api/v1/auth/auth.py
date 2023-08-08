155e6fbf171fc6cb4865aad456c44d72b8c7952e
Ngece <ngecemlamli@gmail.com>
2023-08-08 23:50:40 +0200

    committing auth first task with more documentation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
-"""authantication module"""
-from flask import request
-from typing import List, TypeVar
+"""Authentication Module
+This module provides a class for handling authentication related tasks.
+"""
 
-"""auth class"""
+from flask import request
+from typing import List, TypeVar, Optional
+from os import getenv
 class auth:
-    """auth class"""
+    """Authentication class
+    This class provides methods for handling authentication tasks.
+    """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """require_auth method"""
+        """Check if authentication is required
+        Args:
+            path (str): The request path.
+            excluded_paths (List[str]): List of paths that are excluded from authentication.
+        Returns:
+            bool: True if authentication is required, False if excluded.
+        """
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
         if path[-1] != '/':
@@ -15,15 +26,23 @@ class auth:
         if path in excluded_paths:
             return False
         return True
-    
+            
     def authorization_header(self, request=None) -> str:
-        """authorization_header method"""
+        """Get the Authorization header from the request
+        Args:
+            request: The Flask request object.
+        Returns:
+            str: The value of the Authorization header.
+        """
         if request is None:
             return None
         return request.headers.get('Authorization', None)
     
     def current_user(self, request=None) -> TypeVar('User'):
-        """current_user method"""
+        """Get the current user
+        Args:
+            request: The Flask request object.
+        Returns:
+            TypeVar('User'): The current user object.
+        """
         return None
-    
-    
