8ee47820af2eb18464d7c9da526f4d9e570470ff
Ngece <ngecemlamli@gmail.com>
2023-08-08 23:34:18 +0200

    committing auth first task

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,24 +8,24 @@ from typing import List, TypeVar
 class auth:
     """auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """require_auth method"""
-
-        if path is None or excluded_paths is None or excluded_paths == []:
+        """ Checks if API routes require authentication """
+        if path is None or not excluded_paths:
             return True
-        if path[-1] != '/':
-            path = path + '/'
-        if path in excluded_paths:
-            return False
+        for i in excluded_paths:
+            if i.endswith('*') and path.startswith(i[:-1]):
+                return False
+            elif i in {path, path + '/'}:
+                return False
         return True
-    
+
     def authorization_header(self, request=None) -> str:
-        """authorization_header method"""
-        
-        if request is None:
+        """ Checks if Authorization request header is present
+        & contains values """
+        if request is None or "Authorization" not in request.headers:
             return None
-        return request.headers.get('Authorization', None)
-    
+        else:
+            return request.headers.get('Authorization')
+
     def current_user(self, request=None) -> TypeVar('User'):
-        """current_user method"""
+        """ placeholder """
         return None
-    
