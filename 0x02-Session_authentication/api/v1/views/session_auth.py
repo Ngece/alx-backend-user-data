42a02e0439edcd0b70f8ad9bcf812b4abaee101d
Ngece <ngecemlamli@gmail.com>
2023-08-10 00:26:20 +0200

    committing with POST /auth_session/login route

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""handles all routes for the Session authentication."""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
+from api.v1.app import auth
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /api/v1/auth_session/login
+    Return:
+      - User object JSON represented
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or email == '':
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == '':
+        return jsonify({"error": "password missing"}), 400
+    users = User.search({'email': email})
+    if users == []:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+        from api.v1.app import auth
+        session_id = auth.create_session(user.id)
+        response = jsonify(user.to_json())
+        response.set_cookie(getenv('SESSION_NAME'), session_id)
+        return response
+    
