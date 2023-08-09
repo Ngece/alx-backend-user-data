6901ec055e8a0068123c50857b2804d94457a79e
Ngece <ngecemlamli@gmail.com>
2023-08-10 01:39:42 +0200

    committing user sessions with

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -11,3 +11,16 @@ class UserSession(Base):
         super().__init__(*args, **kwargs)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
+
+    def to_dict(self) -> dict:
+        """ Dictionary representation of User Session"""
+        dictionary = super().to_dict()
+        dictionary['user_id'] = self.user_id
+        dictionary['session_id'] = self.session_id
+        return dictionary
+    
+    def save(self):
+        """ Save the instance to the database"""
+        from models import db_session
+        db_session.add(self)
+        db_session.commit()
