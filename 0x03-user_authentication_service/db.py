280d9fcc56107b9399c5294c341e731adbe3f707
Ngece <ngecemlamli@gmail.com>
2023-08-16 23:53:46 +0200

    committing add user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,10 +29,13 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user instance to the session DB"""
+
+    def add_user(self, email: str, hashed_password: str) -> None:
+        """Method that saves a new user to the database
+        """
+        from user import User
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
-        return new_user
+        return None
+    
