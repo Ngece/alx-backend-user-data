e2db46aa8f37d4b927e02c60aef9ea5bb7195b55
Ngece <ngecemlamli@gmail.com>
2023-08-17 23:24:24 +0200

    committing default route with app.py file

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Route module for the API"""
 
-from flask import Flask, jsonify, request, abort, redirect
+from flask import Flask, jsonify, request, abort, Response
+from flask import Flask, redirect, url_for, session
 from auth import Auth
 from flask.helpers import make_response
 from typing import List, Dict
@@ -11,10 +12,22 @@ app = Flask(__name__)
 AUTH = Auth()
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def welcome() -> str:
+def welcome() -> Response:
     """Welcome message for the API"""
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> Response:
+    """Register a user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    
 
+"""main"""
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
