f15097d45b86289cab096a0038f8821253072e0d
Ngece <ngecemlamli@gmail.com>
2023-08-18 00:52:50 +0200

    committing the app.py module

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,13 +1,82 @@
 #!/usr/bin/env python3
-"""Route module for the API"""
-from flask import Flask, jsonify
+"""Main file"""
+
+from flask import Flask, request, abort, redirect
+from auth import Auth
+import flask
 
 app = Flask(__name__)
+AUTH = Auth()
+
 
 @app.route('/', methods=['GET'])
 def welcome():
-    """Welcome message"""
-    return jsonify({"message": "Bienvenue"})
+    """Return a welcome message"""
+    return flask.jsonify({"message": "Bienvenue"})
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """Return a welcome message"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return flask.jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return flask.jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Return a welcome message"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = flask.jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Return a welcome message"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """Return a welcome message"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return flask.jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """Return a welcome message"""
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        payload = {"email": email, "message": "Password updated"}
+        return flask.jsonify(payload), 200
+    except Exception:
+        abort(403)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
