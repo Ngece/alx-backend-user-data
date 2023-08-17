d6d0f1afb3cd05f093bdc8cf86561be272024d8b
Ngece <ngecemlamli@gmail.com>
2023-08-17 22:34:55 +0200

    committing hash password with auth.py file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,3 +11,20 @@ def _hash_password(password: str) -> bytes:
     """Hashes password"""
     import bcrypt
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generates a UUID"""
+    return str(uuid4())
+
+
+def register_user(email: str, password: str) -> User:
+    """Registers a new user"""
+    db = DB()
+    try:
+        user = db.find_user_by(email=email)
+        raise ValueError(f'User {email} already exists')
+    except NoResultFound:
+        hashed_password = _hash_password(password)
+        user = db.add_user(email, hashed_password)
+        return user
